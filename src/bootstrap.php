92a7f9e2702a13991bcb49357f814002061ca062
Steve Buzonas <steve@fancyguy.com>
2013-03-15 13:30:59 -0400

    fixed parsing error

--- src/bootstrap.php
+++ src/bootstrap.php
@@ -1,11 +1,8 @@
 <?php
-
 include 'bootstrap-functions.php';
 
-$rootAutoload = __DIR__ . '/../vendor/autoload.php';
-$vendorAutoload = __DIR__ . '/../../../autoload.php';
-
-if (!$loader = includeIfExists($rootAutoload) && (!$loader = includeIfExists($vendorAutoload)) {
+if (!$loader = includeIfExists(__DIR__ . '/../vendor/autoload.php')
+    && (!$loader = includeIfExists(__DIR__ . '/../../../autoload.php'))) {
     die('You must set up the project dependencies, run the following commands:' . PHP_EOL .
         'curl -s http://getcomposer.org/installer | php' . PHP_EOL .
         'php composer.phar install' . PHP_EOL);
